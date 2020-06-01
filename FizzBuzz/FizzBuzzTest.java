b98698fd232375332464421025c01cdc76b8e8cf
deejeon <61816883+deejeon@users.noreply.github.com>
2020-06-01 11:56:31 -0700

    added assignments

---
+++ FizzBuzz/FizzBuzzTest.java
@@ -0,0 +1,12 @@
+public class FizzBuzzTest {
+    public static void main(String[] args) {
+        FizzBuzz test = new FizzBuzz();
+        System.out.println(test.fizzBuzz(3));
+        System.out.println(test.fizzBuzz(6));
+        System.out.println(test.fizzBuzz(5));
+        System.out.println(test.fizzBuzz(10));
+        System.out.println(test.fizzBuzz(15));
+        System.out.println(test.fizzBuzz(30));
+        System.out.println(test.fizzBuzz(2));
+    }
+}
